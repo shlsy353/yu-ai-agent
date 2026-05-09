cecd5879c82180c9a5da7d027539e21571098da2
li-xuexing <204133682@qq.com>
2026-05-09 13:58:39 +0800

    First

---
+++ src/test/java/com/tripo/yuaiagent/YuAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripo.yuaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
